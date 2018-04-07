2b2bd14ca64b1dbda55e8e42ebf7532eee6e57e4
gator2018 <roel@roels-macbook-pro.local>
2018-04-07 00:23:34 -0500

    Initial Commit

---
+++ Cereal101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cereal101
+//
+//  Created by Roel Tijerina on 4/7/18.
+//  Copyright © 2018 Roel Tijerina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
